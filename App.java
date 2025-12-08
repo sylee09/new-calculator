bde80ed93399ae58b310db8d7b3cbf6a07dc4076
Seungyoung <leesy960909@gmail.com>
2025-12-08 10:17:23 +0900

    Calculator.java를 App.java로 이름 변경

--- App.java
+++ App.java
@@ -1,7 +1,6 @@
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Calculator {
+public class App {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
